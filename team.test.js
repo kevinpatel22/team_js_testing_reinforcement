d5b2fcc719fabfa405fecb3a20de37a53d700c5d
Kevin <mail.kevinpatel@gmail.com>
2019-07-03 15:34:32 -0400

    test passed

--- team.test.js
+++ team.test.js
@@ -1,5 +1,6 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('teamObj creates an object which contains teamname, level and points', () => {
+const team1 = teamObj('Unicron', 10);
+expect(team1).toEqual({ teamName: 'Unicron', level: 10, points: 0 });
+});
