b4b5c7eb147792fc4f786ac1e9282030750a9935
Kevin <mail.kevinpatel@gmail.com>
2019-07-03 15:37:41 -0400

    test passed

--- team.test.js
+++ team.test.js
@@ -4,3 +4,8 @@ test('teamObj creates an object which contains teamname, level and points', () =
 const team1 = teamObj('Unicron', 10);
 expect(team1).toEqual({ teamName: 'Unicron', level: 10, points: 0 });
 });
+
+test('returns teambj as undefined teamName and level  ', () => {
+  const result = teamObj();
+  expect(result).toEqual({'teamName':undefined, 'level':undefined, 'points':0});
+});
